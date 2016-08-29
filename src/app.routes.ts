cf9be756000788f49b13222115e661bb67be5c45
insujung <wjddlstn0614@naver.com>
2016-08-29 16:04:46 +0900

    라우팅 설정
    Signed-off-by: insujung <wjddlstn0614@naver.com>

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -6,8 +6,12 @@ import { NormalSignup } from './member/normalSignup';
 import { NormalSignupChange } from './member/normalSignupchange';
 import { BusinessSignup } from './member/businessSignup';
 import { AuthGuard } from './common/auth.guard';
-import {MainPage} from './common/mainPage/mainPage';
-import {BuildCaseInput} from './buildCase/buildCaseInput';
+import { MainPage} from './common/mainPage/mainPage';
+import { BuildCaseInput} from './buildCase/buildCaseInput';
+import { ConsultingCounsel} from './consulting/consultingCounsel/consultingCounsel';
+import { ConsultingDetail} from './consulting/consultingDetail/consultingDetail';
+import { ConsultingInfo} from './consulting/consultingInfo/consultingInfo';
+
 
 export const routes: RouterConfig = [
   { path: '',       component:  Login },
@@ -19,6 +23,9 @@ export const routes: RouterConfig = [
   { path: 'buildcaseinput', component: BuildCaseInput },
   { path: 'home',   component: Home, canActivate: [AuthGuard] },
   { path: 'mainPage',   component: MainPage },
+  { path: 'consultingCounsel',   component: ConsultingCounsel },
+  { path: 'consultingDetail',   component: ConsultingDetail },
+  { path: 'consultingInfo',   component: ConsultingInfo },
   { path: '**',     component: Login },
 
 ];
