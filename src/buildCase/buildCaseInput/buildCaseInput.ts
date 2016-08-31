165138f920a80cb90739911ddfbbec2da7e31720
yuwongyu <rkwkblues@gmail.com>
2016-08-31 18:11:10 +0900

    buildCaseInput.ts 에서 서버와 통신 작업중
    Signed-off-by: yuwongyu <rkwkblues@gmail.com>

--- src/buildCase/buildCaseInput/buildCaseInput.ts
+++ src/buildCase/buildCaseInput/buildCaseInput.ts
@@ -17,28 +17,15 @@ const URL = 'http://localhost:3001/api/build-case';
 export class BuildCaseInput {
 
   jwt:string;
-  decodedJwt: string;
+  public decodedJwt;
   public data;
   memberType: string;
 
   constructor(public router: Router, public http: Http, private el:ElementRef) {
     this.jwt = localStorage.getItem('id_token'); //login시 저장된 jwt값 가져오기
     this.decodedJwt = this.jwt && window.jwt_decode(this.jwt);//jwt값 decoding
+    this.memberType = this.decodedJwt.memberType;
     contentHeaders.append('Authorization',this.jwt);//Header에 jwt값 추가하기
-
-    this.http.get('http://localhost:3001/api/user/'+this.decodedJwt.idx, {headers:contentHeaders}) //서버로부터 필요한 값 받아오기
-      .map(res => res.json())//받아온 값을 json형식으로 변경
-      .subscribe(
-        response => {
-          this.data=response; //해당값이 제대로 넘어오는지 확인후 프론트단에 내용 추가
-          this.memberType = this.data.user.memberType;
-        },
-        error => {
-          alert(error.text());
-          console.log(error.text());
-          //서버로부터 응답 실패시 경고창
-        }
-      );
   }
 
   addBuildCase(event, title, buildType, buildPlace, buildTotalArea, buildTotalPrice) {
@@ -54,7 +41,22 @@ export class BuildCaseInput {
     else {
       let body = JSON.stringify({title, buildType, buildPlace, buildTotalArea, buildTotalPrice, HTMLText});
       //html받은 값들을 json형식으로 저장
+/*
+      this.uploader.onBuildItemForm = (item, form) => {
+        form.append(title, title);
+        form.append(buildType, buildType);
+        form.append(buildPlace, buildPlace);
+        form.append(buildTotalArea, buildTotalArea);
+        form.append(buildTotalPrice, buildTotalPrice);
+        form.append(HTMLText, HTMLText);
+      };
 
+      this.uploader.uploadAll();
+      this.uploader.onCompleteItem = (item: any, response: any, status: any, headers: any) => {
+        var responsePath = JSON.parse(response);
+        console.log(response, responsePath);// the url will be in the response
+      };
+*/
       this.http.post('http://localhost:3001/api/build-case', body, {headers: contentHeaders})
         .subscribe(
           response => {
@@ -71,7 +73,11 @@ export class BuildCaseInput {
 
   }
 
-  public uploader:FileUploader = new FileUploader({url: URL});
+  public uploader:FileUploader = new FileUploader({
+    url: URL,
+    headers: contentHeaders
+  });
+
   public hasBaseDropZoneOver:boolean = false;
   public hasAnotherDropZoneOver:boolean = false;
 
