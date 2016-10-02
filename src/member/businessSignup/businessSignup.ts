7c45165402818d5e578989e5a791929f7ce14bcb
ryudy2235 <ryudy2235@naver.com>
2016-10-02 14:34:02 +0900

    회원가입 제한 설정
    폼 입력 필수, email형식 체크, password길이 체크 및 특수문자 제한

--- src/member/businessSignup/businessSignup.ts
+++ src/member/businessSignup/businessSignup.ts
@@ -17,14 +17,30 @@ export class BusinessSignup {
   constructor(public router: Router, public http: Http) {
   }
 
+  /*
+   * 회원가입 버튼 눌렀을 경우
+   */
   businesssignup(event, email, password, password_ok, memberType) {
     //html에서의 value값
     var passwords = password;
     var confirmpasswords = password_ok;
 
-    if (passwords != confirmpasswords) {
+    if (!email.match(/^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/)) {
+      // 이메일 형식 체크
+      alert("올바른 이메일을 사용해 주십시오.");
+    }
+    else if (passwords.length < 8) {
+      // 비밀번호 길이 체크
+      alert("비밀번호는 최소 8자리여야 합니다.");
+    }
+    else if (/[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(passwords)) {
+      // 비밀번호 특수문자 체크
+      alert("비밀번호에 특수문자는 사용하실 수 없습니다.");
+    }
+    else if (passwords != confirmpasswords) {
+      //password 일치하는지 점검
       alert("비밀번호가 일치하지 않습니다");
-    }//password 일치하는지 점검
+    }
     else {
       // event.preventDefault();
       let body = JSON.stringify({ email, password,  memberType });
